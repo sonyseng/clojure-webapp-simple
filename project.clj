13b678a143568eba2a0f63a887bf0c2d33236f87
Sony Seng <sonyseng@gmail.com>
2013-10-01 17:45:58 -0500

    Add ring plugin for fast dev. Added Dummy jd file

--- project.clj
+++ project.clj
@@ -8,4 +8,6 @@
                  [compojure "1.1.5"]
                  [ring/ring-jetty-adapter "1.2.0"]
                  [liberator "0.9.0"]]
+  :plugins [[lein-ring "0.8.7"]]
+  :ring {:handler clojure-webapp-simple.core/main-routes}
   :main clojure-webapp-simple.core)
